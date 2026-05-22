dee22dd8488e9b6fe94c07967ec37b32d496e89a
sanket bhojani <sanketbhojani107@gmail.com>
2026-05-22 12:27:42 +0530

    feat: add swagger docs and prepare for Render deployment

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ import env from 'dotenv'
 env.config();
 import './config/db.js'
 import express from 'express'
+import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
 import router from './routes/router.js'
+
+const swaggerDocument = JSON.parse(fs.readFileSync('./swagger.json', 'utf-8'));
+
 const app = express();
 
 const port = +process.env.PORT || 8778;
 
-
+app.use(cors());
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api",router);
 
 app.listen(port , ()=>{
